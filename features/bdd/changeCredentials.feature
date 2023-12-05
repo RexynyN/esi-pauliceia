6ef28673fff20cc49b72542646ffa0c323abafa8
Aline Murai <66151442+alinemurai@users.noreply.github.com>
2023-12-05 19:31:39 -0300

    adicionando o change credentials

--- features/bdd/changeCredentials.feature
+++ features/bdd/changeCredentials.feature
@@ -5,6 +5,9 @@ O usuario do Pauliceia
 Deseja alterar o nome e o username
 Para conseguir logar sem outro username
 
+Contexto:
+    Dado que o usuario esteja logado
+
 Cenario: Mudar o nome e o username
 Dado que o usuario queira alterar o nome e o username
 Quando ele digitar o nome e o username valido e clicar em submit
---
+++ features/step_definitions/changeCredentials.step.rb
@@ -0,0 +1,21 @@
+Dado('que o usuario esteja logado') do
+    @login = LoginPage.new
+    @prof = ProfilePage.new
+    @prof.load
+end
+
+Dado('que o usuario queira alterar o nome e o username') do
+    @login.userLogin(CREDENTIAL[:user][:email], CREDENTIAL[:user][:password])
+end
+
+Quando('ele digitar o nome e o username valido e clicar em submit') do
+    @prof.testNameChanges
+end
+ 
+Entao('deve ser alterado o nome e o username') do
+    # Ele vai tão rápido que as mudanças nn tem tempo pra processar
+    sleep 2
+    @prof.load
+    expect(@prof).to have_secret_value 
+    @prof.nameChanger(CREDENTIAL[:user][:name], CREDENTIAL[:user][:username])
+end
